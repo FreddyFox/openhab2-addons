39be5933468d02a2fbd659b20eeb4402fb891bf7
Frederik Leira <frederik.leira@gmail.com>
2018-12-07 17:25:23 +0100

    BroadlinkSocketModel2Handler.java: refactor to accomodate night light channel for SP3 devices

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkSocketModel2Handler.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkSocketModel2Handler.java
@@ -9,10 +9,13 @@
 package org.openhab.binding.broadlink.handler;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import javax.crypto.spec.IvParameterSpec;
 
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.thing.*;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.openhab.binding.broadlink.internal.Hex;
 import org.openhab.binding.broadlink.internal.Utils;
 import org.slf4j.Logger;
@@ -26,21 +29,33 @@ import org.slf4j.LoggerFactory;
 public class BroadlinkSocketModel2Handler extends BroadlinkSocketHandler {
 
     private final Logger logger = LoggerFactory.getLogger(BroadlinkSocketModel2Handler.class);
-
+    
     public BroadlinkSocketModel2Handler(Thing thing) {
         super(thing);
     }
-
+    
     protected void setStatusOnDevice(int status) {
         byte payload[] = new byte[16];
         payload[0] = 2;
         payload[4] = (byte) status;
         byte message[] = buildMessage((byte) 106, payload);
         sendDatagram(message);
-	receiveDatagram("acknowledgment packet");
+        receiveDatagram("acknowledgment packet");
     }
 
     protected boolean getStatusFromDevice() {
+    	byte statusPayload[] = getStatusPayloadFromDevice();
+    	if (statusPayload[0] == (byte) 0xFF)
+    	{
+            return false;
+    	}
+        updateDeviceStateFromStatusPayload(statusPayload);
+    	return true;
+
+    }
+
+    protected byte[] getStatusPayloadFromDevice() {
+    	byte error[] = { (byte) 0xFF };
         try {
             byte payload[] = new byte[16];
             payload[0] = 1;
@@ -49,40 +64,42 @@ public class BroadlinkSocketModel2Handler extends BroadlinkSocketHandler {
             byte response[] = receiveDatagram("status for socket");
             if (response == null) {
                     logError("null response from model 2 status request");
-                    return false;
+                    return error;
             }
-            int error = response[34] | response[35] << 8;
-            if (error != 0) {
+            int error_response = response[34] | response[35] << 8;
+            if (error_response != 0) {
                     logError("Error response from model 2 status request; code: " + error);
-                    return false;
+                    return error;
             }
             IvParameterSpec ivSpec = new IvParameterSpec(Hex.convertHexToBytes(thingConfig.getIV()));
             Map properties = editProperties();
             byte decodedPayload[] = Utils.decrypt(Hex.fromHexString((String) properties.get("key")), ivSpec, Utils.slice(response, 56, 88));
             if (decodedPayload == null) {
-                logError("Null payload in response from model 2 status request");
-                return false;
+            	logError("Null payload in response from model 2 status request");
+                return error;
             }
-            updateState("powerOn", deriveOnOffStateFromPayload(decodedPayload));
-            return true;
+            return decodedPayload;
         } catch (Exception ex) {
-                logError("Exception while getting status from device", ex);
-                return false;
+        	logError("Exception while getting status from device", ex);
+            return error;
         }
     }
+    
+    protected void updateDeviceStateFromStatusPayload(byte[] statusPayload) {
+		updateState("powerOn", derivePowerOnStateFromStatusPayload(statusPayload));	
+    }
 
-    private OnOffType deriveOnOffStateFromPayload(byte[] payload) {
+    protected OnOffType derivePowerOnStateFromStatusPayload(byte[] statusPayload) {
 		// Credit to the Python Broadlink implementation for this:
 		// https://github.com/mjg59/python-broadlink/blob/master/broadlink/__init__.py
 		// Function check_power does more than just check if payload[4] == 1 !
-		byte powerByte = payload[4];
-		if (powerByte == 1 || powerByte == 3 || powerByte == 0xFD) {
-			return OnOffType.ON;
-		}
-		return OnOffType.OFF;
+    	byte powerByte = statusPayload[4];
+    	if (powerByte == 1 || powerByte == 3 || powerByte == 0xFD) {
+    		return OnOffType.ON;
+    	}
+    	return OnOffType.OFF;
     }
-
-
+    
     protected boolean onBroadlinkDeviceBecomingReachable() { 
         return getStatusFromDevice();
     }
